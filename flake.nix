a3ff39b0a06e3da9de7d35685e3cfeba936ea90e
James Walker <walkah@walkah.net>
2022-07-15 23:43:58 +0000

    nix flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Fission CLI";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-22.05";
+    utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        naersk-lib = naersk.lib."${system}";
+      in
+      rec {
+        # `nix build`
+        packages.fission = naersk-lib.buildPackage {
+          pname = "fission";
+          root = ./.;
+        };
+        packages.default = packages.fission;
+
+        # `nix run`
+        apps.fission = utils.lib.mkApp {
+          drv = packages.fission;
+        };
+        apps.default = apps.fission;
+
+        # `nix develop`
+        devShells.default = pkgs.mkShell {
+          name = "fission";
+          nativeBuildInputs = with pkgs; [ rustup ];
+        };
+      });
+}
