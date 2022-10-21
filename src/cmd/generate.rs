524e32c7be14a1501d8fb118e48b9716d6cb1969
Savannah Jackson <savannah.jackson@tutanota.com>
2022-10-21 15:14:25 -0500

    adding generate credentials

--- src/cmd/generate.rs
+++ src/cmd/generate.rs
@@ -1,10 +1,16 @@
 use clap::{Args, Subcommand};
+use ed25519_zebra::*;
+use rand::thread_rng;
+use colored::Colorize;
 
 #[derive(Args)]
 pub struct Generate {
     #[clap(subcommand)]
     command: GenerateCommands,
 }
+
+const DID_KEY_PREFIX:&str = "did:key:";
+const DID_KEY_PREFIX_ED25519:&str = "z6M";
 
 #[derive(Subcommand)]
 pub enum GenerateCommands {
@@ -14,7 +20,20 @@ pub enum GenerateCommands {
 pub fn run_command(g: Generate) {
     match g.command {
         GenerateCommands::Credentials => {
-            todo!("credentials")
+            let private_key = SigningKey::new(thread_rng());
+            let public_key = VerificationKey::from(&private_key);
+            
+            let private_key_bytes: [u8; 32] = private_key.into();
+            let public_key_bytes: [u8; 32] = public_key.into();
+
+            let did_data = bs58::encode(&public_key_bytes).into_string();
+
+            let did = format!("{}{}{}", DID_KEY_PREFIX, DID_KEY_PREFIX_ED25519, did_data);
+
+            println!("{}", "✅ Generated an Ed25519 key pair and associated DID".bright_green());
+            println!("🗝️  Private key: {}", base64::encode(&private_key_bytes).bright_blue());
+            println!("🔑 Public key: {}", base64::encode(&public_key_bytes).bright_blue());
+            println!("🆔 DID: {}", did.bright_blue());
         }
     }
 }
