3e5e7704baaa38cafab2f2c023bd9588be364600
Savannah Jackson <savannah.jackson@tutanota.com>
2022-10-28 18:52:06 -0500

    IPFS Trait Complete

--- src/ipfs.rs
+++ src/ipfs.rs
@@ -1,40 +1,25 @@
+//use std::collections::{HashMap, btree_map::OccupiedError};
+
 use std::collections::HashMap;
 
-use serde_json::Value;
-
-use self::{error::IpfsError, result::*};
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
 
 pub mod daemon;
-pub mod error;
-pub mod result;
 
 pub trait Ipfs {
-    fn add(name:&str, path:&str, is_directory:bool) -> Result<AddedData, IpfsError>;
-    fn bitswap_ledger(peer_id:&str) -> Result<Ledger, IpfsError>;
-    fn bitswap_reprovide() -> Result<String, IpfsError>;
-    //Note bitswap_stat is left out because it for diagonostic purposes
-    fn bitswap_wantlist(peer: &str) ->  Result<HashMap<String, String>, IpfsError>;
-    fn get_block(block_cid: &str) -> Result<String, IpfsError>;
-    //TODO: multihash_alg should be enum
-    fn put_block(data: &str, multihash_alg: &str, multihash_length: &str, is_pin:bool, allow_big_block:bool, cid_codec:Option<String>)
-        -> Result<IpfsBlock, IpfsError>;
-    fn remove_block(block_cid:&str, force:bool) -> Result<String, IpfsError>;
-    fn block_stats(block_cid:&str) -> Result<IpfsBlock, IpfsError>;
-    fn get_bootstrap_peers() -> Result<Vec<String>, IpfsError>;
-    fn add_bootstrap_peer(peer:&str) -> Result<Vec<String>, IpfsError>;
-    fn add_bootstrap_defaults() -> Result<Vec<String>, IpfsError>;
-    fn remove_bootstrap_peer(peer:&str) -> Result<Vec<String>, IpfsError>;
-    fn remove_all_bootstrap_peer(peer:&str) -> Result<Vec<String>, IpfsError>;
-    fn get_object_data(path:&str, start:Option<i64>, length:Option<i64>)
-        -> Result<String, IpfsError>;
-    fn cid_to_base32(cid: &str) -> Result<String, IpfsError>;
-    //format, bases, hashes, commands, and codes endpoints were not included as I think they are mainly for debug
-    fn edit_config(key:&str, value:Option<&str>, is_bool:bool, is_object:bool) -> Result<Value, IpfsError>; 
-    fn apply_profile_config(profile:&str) -> Result<Value, IpfsError>;
-    fn replace_config(path:&str) -> Result<String, IpfsError>;
-    fn get_config() -> Result<Value, IpfsError>;
-    fn export_dag(cid: &str) -> Result<String, IpfsError>;
-    //TODO: format should be enum
-    fn get_dag(cid: &str, codec: &str) -> Result<String, IpfsError>;
-    fn import_dag(path:&str, pin_roots:bool, allow_big_block:bool) -> Result<(), IpfsError>;
+    fn add_file(path:&str) -> Result<CidData>;
+    fn add_directory(path:&str) -> Result<CidData>;
+    fn add_bootstrap(peer_id:&str) -> Result<Vec<String>>;
+    fn connect_to(peer_id:&str) -> Result<Vec<String>>;
+    fn disconect_from(peer_id:&str)-> Result<Vec<String>>;
+    fn config(options:HashMap<String, String>) -> Result<HashMap<String, String>>;
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CidData{
+    bytes:i64,
+    hash:String,
+    name:String,
+    size:String
 }
--- src/ipfs/daemon.rs
+++ src/ipfs/daemon.rs
@@ -1,97 +1,37 @@
 use crate::ipfs::Ipfs;
-use super::{error::IpfsError, result::*};
+use super::CidData;
+use anyhow::Result;
 use std::collections::HashMap;
 pub struct IpfsViaDaemon {
     
 }
+impl IpfsViaDaemon {
+    pub fn new() -> Result<IpfsViaDaemon> {
+        todo!()
+    }
+}
 impl Ipfs for IpfsViaDaemon {
-    fn add(name:&str, path:&str, is_directory:bool) -> Result<AddedData, IpfsError> {
+    fn add_file(path:&str) -> Result<CidData> {
         todo!()
     }
 
-    fn bitswap_ledger(peer_id:&str) -> Result<Ledger, IpfsError> {
+    fn add_directory(path:&str) -> Result<CidData> {
         todo!()
     }
 
-    fn bitswap_reprovide() -> Result<String, IpfsError> {
+    fn add_bootstrap(peer_id:&str) -> Result<Vec<String>> {
         todo!()
     }
 
-    fn bitswap_wantlist(peer: &str) ->  Result<HashMap<String, String>, IpfsError> {
+    fn connect_to(peer_id:&str) -> Result<Vec<String>> {
         todo!()
     }
 
-    fn get_block(block_cid: &str) -> Result<String, IpfsError> {
+    fn disconect_from(peer_id:&str)-> Result<Vec<String>> {
         todo!()
     }
 
-    fn put_block(data: &str, multihash_alg: &str, multihash_length: &str, is_pin:bool, allow_big_block:bool, cid_codec:Option<String>)
-        -> Result<IpfsBlock, IpfsError> {
-        todo!()
-    }
-
-    fn remove_block(block_cid:&str, force:bool) -> Result<String, IpfsError> {
-        todo!()
-    }
-
-    fn block_stats(block_cid:&str) -> Result<IpfsBlock, IpfsError> {
-        todo!()
-    }
-
-    fn get_bootstrap_peers() -> Result<Vec<String>, IpfsError> {
-        todo!()
-    }
-
-    fn add_bootstrap_peer(peer:&str) -> Result<Vec<String>, IpfsError> {
-        todo!()
-    }
-
-    fn add_bootstrap_defaults() -> Result<Vec<String>, IpfsError> {
-        todo!()
-    }
-
-    fn remove_bootstrap_peer(peer:&str) -> Result<Vec<String>, IpfsError> {
-        todo!()
-    }
-
-    fn remove_all_bootstrap_peer(peer:&str) -> Result<Vec<String>, IpfsError> {
-        todo!()
-    }
-
-    fn get_object_data(path:&str, start:Option<i64>, length:Option<i64>)
-        -> Result<String, IpfsError> {
-        todo!()
-    }
-
-    fn cid_to_base32(cid: &str) -> Result<String, IpfsError> {
-        todo!()
-    }
-
-    fn edit_config(key:&str, value:Option<&str>, is_bool:bool, is_object:bool) -> Result<serde_json::Value, IpfsError> {
-        todo!()
-    }
-
-    fn apply_profile_config(profile:&str) -> Result<serde_json::Value, IpfsError> {
-        todo!()
-    }
-
-    fn replace_config(path:&str) -> Result<String, IpfsError> {
-        todo!()
-    }
-
-    fn get_config() -> Result<serde_json::Value, IpfsError> {
-        todo!()
-    }
-
-    fn export_dag(cid: &str) -> Result<String, IpfsError> {
-        todo!()
-    }
-
-    fn get_dag(cid: &str, codec: &str) -> Result<String, IpfsError> {
-        todo!()
-    }
-
-    fn import_dag(path:&str, pin_roots:bool, allow_big_block:bool) -> Result<(), IpfsError> {
+    fn config(options:HashMap<String, String>) -> Result<HashMap<String, String>> {
         todo!()
     }
 }
