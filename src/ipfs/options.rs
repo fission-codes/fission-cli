31c0dee0f8db7eae7759ee550d40ad3fa179fc1a
Savannah Jackson <savannah.jackson@tutanota.com>
2022-11-30 21:18:20 -0600

    fixing config

--- src/ipfs/options.rs
+++ src/ipfs/options.rs
@@ -1,5 +1,5 @@
 pub const IPFS_API_PORT:u16 = 4869;
-pub const IPFS_RETRY_ATTEMPTS:u16 = 7;
+pub const IPFS_RETRY_ATTEMPTS:u16 = 4;
 pub const IPFS_ADDR:&str = "127.0.0.1";
 pub const IPFS_EXE:&str = "ipfs";
 pub const HTTP_BOUNDARY:&str = "------------------------I_am_a_boundary_123A123";
--- src/utils/json.rs
+++ src/utils/json.rs
@@ -1,12 +1,11 @@
+use serde_json::{Map, Value};
+use anyhow::{Result, bail};
+
 /*
 Ipfs often returns json with a single property with the value being an array. 
 This function simply takes the array in that property and turns it into a vector that can be used in rust.
 It will return an error result if the json is not formatted in this way.
 */
-
-use serde_json::Value;
-use anyhow::{Result, bail};
-
 pub fn value_to_vec<A>(json:&Value, index:&str) -> Result<Vec<A>> 
     where A:Clone + for<'de> serde::Deserialize<'de>{
     anyhow::Ok( match json.get(index) {
@@ -27,4 +26,31 @@ pub fn value_to_vec<A>(json:&Value, index:&str) -> Result<Vec<A>>
         },
         None => bail!("Improperly formatted Json: Cant locate index {}", index)
     })   
+}
+
+pub fn change_json_part<I>(root:&Value, loc:I, to:&Value) -> Result<Value>
+    where I: Iterator<Item = String> + Clone{
+    if loc.clone().count() == 0 {
+        // println!("{}", "found it!".red());
+        return Ok(to.clone());
+    }else if !root.is_object(){
+        return Ok(root.clone());
+    }else{
+        let root_iter = match root.as_object() {
+            Some(o) => o.iter(),
+            None => return Ok(Value::Object(serde_json::Map::new()))
+        };
+        let mut new_root:Map<String, Value> = Map::new();
+        for (prop, val) in root_iter {
+            let mut new_loc = loc.clone();
+            let prop_to_match = new_loc.next().unwrap().to_string() ;
+            // println!("{} <=> {} = {}", prop, prop_to_match, prop.to_owned() == prop_to_match);
+            let new_val = match prop.to_owned() == prop_to_match{
+                true => change_json_part(val, new_loc, to)?,
+                false => change_json_part(val, loc.clone(), to)?
+            };
+            new_root.insert(prop.to_owned(), new_val);
+        }
+        return Ok(Value::Object(new_root));
+    }
 }
